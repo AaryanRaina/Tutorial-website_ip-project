553bdbf7b8de7cd0db2756d992dcc9b6cfe035fa
Saurabh Jena <p17saurabhj@iimsambalpur.ac.in>
2020-12-10 16:20:39 +0530

    Qpaper and RB after login

---
+++ RFafterlogin.php
@@ -0,0 +1,164 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Reference Books</title>
+	<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="Keywords" content="College, Pokhara, IT College, Lions Marga, college in pokhara, nepalIT">
+ <meta name="author" content="Bishworaj Poudel">
+ <link rel="stylesheet" type="text/css" href="css/style.css">
+ <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<script src="javascript/js.js"></script>
+
+</head>
+<body class="body">
+    <?php
+		require_once('nav.php');
+	?>
+<h1>9th State Board Class</h1>
+<div class="gallery">
+  <a  href="https://drive.google.com/file/d/1zvUwOBj1wKbBFMBRsVQITldit0CaG0tN/view?usp=sharing">
+    <img src="images/english9.PNG" alt="CCT101" width="300" height="200">
+  </a>
+  <div class="desc">English</div>
+</div>
+
+<div class="gallery">
+  <a  href="https://drive.google.com/file/d/1zvUwOBj1wKbBFMBRsVQITldit0CaG0tN/view?usp=sharing">
+    <img src="images/science9.PNG"" alt="Html" width="300" height="200">
+  </a>
+  <div class="desc">Science</div>
+</div>
+
+<div class="gallery">
+  <a  href="https://drive.google.com/file/d/1zvUwOBj1wKbBFMBRsVQITldit0CaG0tN/view?usp=sharing">
+    <img src="images/math.PNG"" alt="Database" width="300" height="200">
+  </a>
+  <div class="desc">Mathematics</div>
+</div>
+
+
+
+<div id="fix"></div>
+
+<hr>
+<h1>9th CBSE Class</h1>
+<div class="gallery">
+  <a  href="https://drive.google.com/file/d/1zvUwOBj1wKbBFMBRsVQITldit0CaG0tN/view?usp=sharing">
+    <img src="images/e.jpg" alt="CCT101" width="300" height="200">
+  </a>
+  <div class="desc">English</div>
+</div>
+
+
+<div class="gallery">
+  <a  href="https://drive.google.com/file/d/1zvUwOBj1wKbBFMBRsVQITldit0CaG0tN/view?usp=sharing">
+    <img src="images/s.jpg"" alt="Html" width="300" height="200">
+  </a>
+  <div class="desc">Science</div>
+</div>
+
+<div class="gallery">
+  <a  href="https://drive.google.com/file/d/1zvUwOBj1wKbBFMBRsVQITldit0CaG0tN/view?usp=sharing">
+    <img src="images/m.jpeg"" alt="Database" width="300" height="200">
+  </a>
+  <div class="desc">Mathematics</div>
+</div>
+
+
+<div id="fix"></div>
+
+<hr>
+<h1>10th State Board Class</h1>
+<div class="gallery">
+  <a  href="https://drive.google.com/file/d/1zvUwOBj1wKbBFMBRsVQITldit0CaG0tN/view?usp=sharing">
+    <img src="images/english9.PNG" alt="CCT101" width="300" height="200">
+  </a>
+  <div class="desc">English</div>
+</div>
+
+
+<div class="gallery">
+  <a  href="https://drive.google.com/file/d/1zvUwOBj1wKbBFMBRsVQITldit0CaG0tN/view?usp=sharing">
+    <img src="images/science9.PNG"" alt="Html" width="300" height="200">
+  </a>
+  <div class="desc">Science</div>
+</div>
+
+<div class="gallery">
+  <a  href="https://drive.google.com/file/d/1zvUwOBj1wKbBFMBRsVQITldit0CaG0tN/view?usp=sharing">
+    <img src="images/math.PNG"" alt="Database" width="300" height="200">
+  </a>
+  <div class="desc">Mathematics</div>
+</div>
+
+<div id="fix"></div>
+
+<hr>
+
+
+<h1>10th CBSE Class</h1>
+<div class="gallery">
+  <a  href="https://drive.google.com/file/d/1zvUwOBj1wKbBFMBRsVQITldit0CaG0tN/view?usp=sharing">
+    <img src="images/en.jpg" alt="CCT101" width="400" height="200">
+  </a>
+  <div class="desc">English</div>
+</div>
+
+
+<div class="gallery">
+  <a  href="https://drive.google.com/file/d/1zvUwOBj1wKbBFMBRsVQITldit0CaG0tN/view?usp=sharing">
+    <img src="images/91gcjeazhvL.jpg"" alt="Html" width="300" height="600">
+  </a>
+  <div class="desc">Science</div>
+</div>
+
+<div class="gallery">
+  <a  href="images/Course/c.jpg">
+    <img src="https://drive.google.com/file/d/1zvUwOBj1wKbBFMBRsVQITldit0CaG0tN/view?usp=sharing"" alt="Database" width="300" height="200">
+  </a>
+  <div class="desc">Mathematics</div>
+</div>
+
+<div id="fix"></div>
+
+<hr>
+
+<script>
+	function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+</script>
+<footer class="mainFooter">
+
+		<footer class="footer-distributed">
+
+			<div class="footer-right">
+
+				<a href="https://www.facebook.com/aaryan.raina.7"><i class="fa fa-facebook"></i></a>
+				<a href="https://twitter.com/"><i class="fa fa-twitter"></i></a>
+				<a href="https://www.instagram.com/aaryan_raina/"><i class="fa fa-instagram"></i></a>
+				<a href="https://www.linkedin.com/in/hemkesh-raina-90a37a146/?originalSubdomain=in"><i class="fa fa-linkedin"></i></a>
+
+			</div>
+
+			<div class="footer-left">
+
+				<p class="footer-links">
+				<a href="JobOppurtunities.php"> Job Opportunities</a>
+				</p>
+
+				<p id="cpy">Venjan Tutorials&copy; 2020<br><br>I am very passionate about sharing my knowledge to the students for free!</p>
+			</div>
+
+		</footer>
+</footer>
+<script type="text/javascript">
+	document.title="Reference Book";
+</script>
+</body>
+</html>
